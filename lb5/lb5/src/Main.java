0e414c6b422d1b82b57247bd85ab9269fb9bea53
ihortetiushev <ihor.tetiushev@gmail.com>
2023-04-19 23:52:30 +0300

    start

--- lb5/lb5/src/Main.java
+++ lb5/lb5/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final int Nn = 27;
+        final int m = 1;
+        final int N = 5;
+
+        double lambda = 15.0 * ((double) m / (Nn * N));
+        double V = 3.0 * ((m + 2.0 + (Nn / 5.0)) / (Nn * N));
+        double p = lambda / V;
+        System.out.println("Task1");
+        System.out.println("lambda = " + lambda);
+        System.out.println("V = " + V);
+        System.out.println("p = " + p);
+
+
+
     }
 }
